eeec1283e10ae33233643c8c77eff8853bade524
Yaroalibou <abdilkadirhalilu01@gmail.com>
2026-04-02 07:16:40 +0000

    Update grass.js

--- client/assets/grass.js
+++ client/assets/grass.js
@@ -1,1 +1,13 @@
+const loader = new THREE.TextureLoader();
 
+const grass = loader.load("assets/grass.jpg");
+grass.wrapS = grass.wrapT = THREE.RepeatWrapping;
+grass.repeat.set(20, 20);
+
+const field = new THREE.Mesh(
+  new THREE.PlaneGeometry(50, 30),
+  new THREE.MeshStandardMaterial({ map: grass })
+);
+
+field.rotation.x = -Math.PI / 2;
+scene.add(field);
